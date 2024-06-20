7d5c1d201ed1005099bb9af896688bb1197db3d0
Gonzo-CSUMB <joshgonzales@csumb.edu>
2024-06-20 11:43:44 -0700

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,21 +1,25 @@
 // axios interceptor, to add the token to the request
-import axios from 'axios';
-import { ACCESS_TOKEN } from './constants';
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
+
+const apiUrl = "/choreo-apis/djangoreact/backend/v1";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, // VITE_API_URL is an environment variable
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl, // VITE_API_URL is an environment variable
 });
 
-api.interceptors.request.use( // Add the token to the request
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`; // embed the token in the header
-        }
-        return config;
-    },
-    (error) => { Promise.reject(error)
+api.interceptors.request.use(
+  // Add the token to the request
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // embed the token in the header
     }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
 );
 
 export default api; // Export the api object, to be used in other requests
